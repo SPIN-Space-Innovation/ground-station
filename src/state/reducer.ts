fb27c6db358fcf14bae97a98f91e803b705c03b2
themicp <themicp@gmail.com>
2022-03-09 18:17:57 +0200

    Ignore debug packets

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -53,54 +53,55 @@ export default function reducer(state: any, action: any) {
       try {
         const packet: TelemetryPacket = JSON.parse(payload);
         const parsedMessage: ParsedMessage = parseTelemetryMessage(packet.message);
+        if (parsedMessage.message_type === 'state') {
+          /*
+          if (parsedMessage.message_type === 'state' && startMET === null) {
+            startMET = parsedMessage.mission_elapsed_time;
+            startTime = Date.now();
+          }
+          const packetTimestamp = startMET && startTime
+            ? startTime + (parsedMessage.mission_elapsed_time - startMET)
+            : Date.now();
+          */
+          const packetTimestamp = Date.now();
 
-        /*
-        if (parsedMessage.message_type === 'state' && startMET === null) {
-          startMET = parsedMessage.mission_elapsed_time;
-          startTime = Date.now();
-        }
-        const packetTimestamp = startMET && startTime
-          ? startTime + (parsedMessage.mission_elapsed_time - startMET)
-          : Date.now();
-        */
-        const packetTimestamp = Date.now();
+          newState.telemetry = {
+            ...newState.telemetry,
+            rssi: [
+              ...newState.telemetry.rssi,
+              { value: packet.rssi, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW),
+            battery_voltage: [
+              ...newState.telemetry.battery_voltage,
+              { value: parsedMessage.battery_voltage, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW),
+            free_memory: [
+              ...newState.telemetry.free_memory,
+              { value: parsedMessage.free_memory, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW),
+          };
 
-        newState.telemetry = {
-          ...newState.telemetry,
-          rssi: [
-            ...newState.telemetry.rssi,
-            { value: packet.rssi, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW),
-          battery_voltage: [
-            ...newState.telemetry.battery_voltage,
-            { value: parsedMessage.battery_voltage, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW),
-          free_memory: [
-            ...newState.telemetry.free_memory,
-            { value: parsedMessage.free_memory, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW),
-        };
+          newState.telemetry.fsmState = parsedMessage.fsm_state;
+          if (parsedMessage.acceleration.x !== null) {
+            newState.telemetry.acceleration = [
+              ...state.telemetry.acceleration,
+              { ...parsedMessage.acceleration, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW);
+          }
 
-        newState.telemetry.fsmState = parsedMessage.fsm_state;
-        if (parsedMessage.acceleration.x !== null) {
-          newState.telemetry.acceleration = [
-            ...state.telemetry.acceleration,
-            { ...parsedMessage.acceleration, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW);
-        }
+          if (parsedMessage.angular_velocity.x !== null) {
+            newState.telemetry.angular_velocity = [
+              ...state.telemetry.angular_velocity,
+              { ...parsedMessage.angular_velocity, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW);
+          }
 
-        if (parsedMessage.angular_velocity.x !== null) {
-          newState.telemetry.angular_velocity = [
-            ...state.telemetry.angular_velocity,
-            { ...parsedMessage.angular_velocity, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW);
-        }
-
-        if (parsedMessage.agl !== null) {
-          newState.telemetry.agl = [
-            ...state.telemetry.agl,
-            { value: parsedMessage.agl / 100, time: packetTimestamp },
-          ].slice(-PACKETS_WINDOW);
+          if (parsedMessage.agl !== null) {
+            newState.telemetry.agl = [
+              ...state.telemetry.agl,
+              { value: parsedMessage.agl / 100, time: packetTimestamp },
+            ].slice(-PACKETS_WINDOW);
+          }
         }
       } catch (e) {
         // eslint-disable-next-line no-console
