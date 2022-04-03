243a2da901f015d7204a9fd20959ef11fd089aaf
themicp <themicp@gmail.com>
2022-04-04 00:35:17 +0300

    Show rocket path in map

--- src/components/RocketLocation.tsx
+++ src/components/RocketLocation.tsx
@@ -2,7 +2,7 @@ import { LatLngTuple, Map } from 'leaflet';
 import React from 'react';
 import _ from 'lodash';
 import {
-  MapContainer, Marker, Popup, TileLayer,
+  MapContainer, Marker, Polyline, Popup, TileLayer,
 } from 'react-leaflet';
 import Context from '../state/Context';
 
@@ -11,6 +11,7 @@ import Context from '../state/Context';
 export default function RocketLocation() {
   const { state } = React.useContext(Context);
   const [position, setPosition] = React.useState<LatLngTuple>([0, 0]);
+  const [path, setPath] = React.useState<any>([]);
   const [map, setMap] = React.useState<Map | null>(null);
 
   const whenMapCreated = (mapInstance: Map) => {
@@ -26,6 +27,12 @@ export default function RocketLocation() {
     if (map) {
       map.panTo(lastPosition);
     }
+
+    if (lastPosition[0] === 0 || lastPosition[1] === 0) {
+      setPath([]);
+    } else {
+      setPath([...path, [...lastPosition]]);
+    }
   }, [state.position]);
 
   return (
@@ -34,6 +41,7 @@ export default function RocketLocation() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Polyline positions={path} pathOptions={{ color: 'red' }} />
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup.
